a9712961ace4dae216de3c651a8170c8d29fb58f
Thalia Nero <chrisvn00@gmail.com>
2020-03-31 01:18:45 -0400

    Mod template

---
+++ src/main/java/io/github/kvverti/mobcycle/MobCycle.java
@@ -0,0 +1,11 @@
+package io.github.kvverti.mobcycle;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MobCycle implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
